26ceb066db231dc72f8b5fea32fdf0e4cdd5e31f
dfe325 <daneder@gmail.com>
2020-04-03 08:29:12 -0400

    Chap 15

--- articles/admin.py
+++ articles/admin.py
@@ -1,7 +1,16 @@
-# articles/admin.py 
+# articles/admin.py
 
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Comment #  new
 
-admin.site.register(Article)
+class CommentInline(admin.TabularInline): # new
+    model = Comment
+
+class ArticleAdmin(admin.ModelAdmin): # new
+    inlines = [
+        CommentInline,
+    ]
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)  #  new
